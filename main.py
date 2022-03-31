1fab7917413200909e9d6929a908c1e985f7e975
Guilherme Helton <76913525+guilhermehelton@users.noreply.github.com>
2022-03-31 23:35:43 +0000

    joke function

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import discord
+import os
+import requests
+import json
+
 
 client = discord.Client()
+TOKEN = os.environ['TOKEN']
+
+def get_joke():
+	response = requests.get('https://v2.jokeapi.dev/joke/Programming?type=single')
+	json_data = json.loads(response.text)
+	joke = f"Type: {json_data['category']} \nJoke: {json_data['joke']}"
+	return joke
 
 @client.event
 async def on_ready():
@@ -8,4 +19,11 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-	
+	if message.author == client.user:
+		return
+
+	if message.content.startswith('$joke'):
+		joke = get_joke()
+		await message.channel.send(joke)
+
+client.run(TOKEN)
